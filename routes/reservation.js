7067fb67bd72c8dcd1625bd1b2e7cfb9fb42d048
strel2k10 <areias.tiago1502@gmail.com>
2020-05-24 23:49:47 +0100

    hello world

---
+++ routes/reservation.js
@@ -0,0 +1,19 @@
+module.exports = app =>{
+    const reservation = require("../controllers/reservation.js")
+
+   // app.get('/reservations/:idReservation', reservation.findById)
+
+   // app.get('/restaurants/:idRestaurant/reservations', reservation.findByRestaurant)
+
+   // app.get('/users/:idUser/reservations', reservation.findByUser)
+
+   // app.post('/tables/:idTable/users/:idUser/reservations', reservation.create)
+
+   // app.put('/reservations/:idReservation', reservation.update)
+
+   // app.put('/reservations/:idReservation/confirm',reservation.confirm)
+
+   // app.delete('/reservations/:idReservation', reservation.deleteById)
+
+   // app.delete('/restaurants/:idRestaurant/reservations', reservation.deleteByRestaurant)
+}
